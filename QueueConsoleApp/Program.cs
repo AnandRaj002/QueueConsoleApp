4312acf251e64cbf13c11b18f3b5fc27d94d49d9
Anand Rajpara <anand.rajpara@outlook.com>
2021-01-13 00:08:09 +0530

    Add project files.

---
+++ QueueConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generic Class Queue in C#
+            Queue<int> queue = new Queue<int>();
+            int count = 0;
+
+            Console.WriteLine("Queue Generic Class");
+
+            Console.WriteLine("How many element needed in Queue");
+            var inputCount = Console.ReadLine();
+
+            if(Int32.TryParse(inputCount, out count))
+            {
+                for(int i=0;i<count;i++)
+                {
+                    queue.Enqueue(Convert.ToInt32(Console.ReadLine()));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Number of elements in Queue is {queue.Count}");
+            Console.WriteLine($"Latest element in Queue is {queue.Peek()} - Queue PeeK Operation");
+
+            while(queue.Count > 0)
+            {
+                Console.WriteLine($"Element in Queue is {queue.Dequeue()} - Queue Dequeue Operation");
+            }
+
+            // Queue with Linked List
+            Console.WriteLine();
+            QueueLinkedList queueLinkedList = new QueueLinkedList();
+            Console.WriteLine("How many element needed in Queue");
+            var inputListCount = Console.ReadLine();
+
+            if (Int32.TryParse(inputListCount, out count))
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    queueLinkedList.Enqueue(Convert.ToInt32(Console.ReadLine()));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Latest element in Queue is {queueLinkedList.PeeK()} - Queue PeeK Operation");
+
+            while (queueLinkedList.first != null)
+            {
+                Console.WriteLine($"Element in Queue is {queueLinkedList.Dequeue()} - Queue Dequeue Operation");
+            }
+
+        }
+    }
+}
---
+++ QueueConsoleApp/QueueLinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace QueueConsoleApp
+{
+    public class Node
+    {
+        public int data;
+        public Node link;
+    }
+
+    public class QueueLinkedList
+    {
+        public Node first, last;
+
+        public QueueLinkedList()
+        {
+            this.first = null;
+            this.last = null;
+        }
+
+        public void Enqueue(int x)
+        {
+            Node node = new Node();
+            node.data = x;
+
+            if (first == null)
+            {
+                first = last = node;
+            }
+
+            last.link = node;
+            last = node;
+        }
+
+        public int PeeK()
+        {
+            if((first != null))
+            {
+                return first.data;
+            }
+            else
+            {
+                Console.WriteLine("Queue is Empty");
+                return -1;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if(first != null)
+            {
+                int data = first.data;
+                first = first.link;
+                return data;
+            }
+            else
+            {
+                Console.WriteLine("Queue is Empty");
+                return -1;
+            }
+        }
+    }
+}
